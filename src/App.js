bffce38b659a02927f0633ab2275c089075ce7a0
aman verma <amanv3093@gmai.com>
2024-03-01 21:01:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Quote from './Components/Quote';
+
+import './index.css';
+function App() {
+  return (
+    <div >
+     <Header />
+  <Quote />
+    <Footer />
+
+    </div>
+  );
+}
+
+export default App;
